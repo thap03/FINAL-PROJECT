6a4cb1b48687712c85f0d06a01c8acb1159e7ca0
thap03 <112776022+thap03@users.noreply.github.com>
2022-12-16 16:12:17 -0800

    Update sketchTicTacToe.ino
    updated

--- scripts/sketchTicTacToe.ino
+++ scripts/sketchTicTacToe.ino
@@ -6,17 +6,17 @@
 #include <time.h>
 
 
-#define LED_RED1  0
+#define LED_RED1 0
 #define LED_RED2 15
 #define LED_RED3 5
 
 #define LED_BLUE1 2
-//#define LED_BLUE2 
-//#define LED_BLUE3 
+#define LED_BLUE2 18
+#define LED_BLUE3 19
 
-//#define LED_GREEN1 
-//#define LED_GREEN2 
-//#define LED_GREEN3 
+#define LED_GREEN1 12
+#define LED_GREEN2 14
+#define LED_GREEN3 32
 
 void setup() {
   pinMode(LED_RED1, OUTPUT);
@@ -24,12 +24,12 @@ void setup() {
   pinMode(LED_RED3, OUTPUT);
 
   pinMode(LED_BLUE1, OUTPUT);
-  //pinMode(LED_BLUE2, OUTPUT);
-  //pinMode(LED_BLUE3, OUTPUT);
+  pinMode(LED_BLUE2, OUTPUT);
+  pinMode(LED_BLUE3, OUTPUT);
 
-  //pinMode(LED_GREEN1, OUTPUT);
-  //pinMode(LED_GREEN2, OUTPUT);
-  //pinMode(LED_GREEN3, OUTPUT);
+  pinMode(LED_GREEN1, OUTPUT);
+  pinMode(LED_GREEN2, OUTPUT);
+  pinMode(LED_GREEN3, OUTPUT);
 
 }
 
@@ -40,12 +40,12 @@ void loop() {
   digitalWrite(LED_RED3, HIGH);
   
   digitalWrite(LED_BLUE1, HIGH); 
-  //digitalWrite(LED_BLUE2, HIGH);
-  //digitalWrite(LED_BLUE3, HIGH);
+  digitalWrite(LED_BLUE2, HIGH);
+  digitalWrite(LED_BLUE3, HIGH);
 
-  //digitalWrite(LED_GREEN1, HIGH); 
-  //digitalWrite(LED_GREEN2, HIGH);
-  //digitalWrite(LED_GREEN3, HIGH);
+  digitalWrite(LED_GREEN1, HIGH); 
+  digitalWrite(LED_GREEN2, HIGH);
+  digitalWrite(LED_GREEN3, HIGH);
 
   delay(1000);  
 
@@ -54,12 +54,12 @@ void loop() {
   digitalWrite(LED_RED3, LOW);
 
   digitalWrite(LED_BLUE1, LOW);   
- // digitalWrite(LED_BLUE2, LOW);
- //digitalWrite(LED_BLUE3, LOW);
+  digitalWrite(LED_BLUE2, LOW);
+  digitalWrite(LED_BLUE3, LOW);
 
-  //digitalWrite(LED_GREEN1, LOW); 
-  //digitalWrite(LED_GREEN2, LOW);
-  //digitalWrite(LED_GREEN3, LOW);
+  digitalWrite(LED_GREEN1, LOW); 
+  digitalWrite(LED_GREEN2, LOW);
+  digitalWrite(LED_GREEN3, LOW);
 
   delay(1000);                       
 }
