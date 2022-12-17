0a447f74648c9148380f242563c6bc7acedfb62e
thap03 <112776022+thap03@users.noreply.github.com>
2022-12-16 23:19:06 -0800

    Update sketchTicTacToe.ino

--- scripts/sketchTicTacToe.ino
+++ scripts/sketchTicTacToe.ino
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 #define LED_RED1 0
 #define LED_RED2 15
 #define LED_RED3 5
@@ -17,6 +16,17 @@
 #define LED_GREEN1 12
 #define LED_GREEN2 14
 #define LED_GREEN3 32
+
+const char *ssid = "gen2"; // Enter your WiFi name
+const char *password = "substitute";  // Enter WiFi password
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "esp32/TicTacToe2";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
   pinMode(LED_RED1, OUTPUT);
@@ -33,6 +43,42 @@ void setup() {
 
 }
 
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()){
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+        if (client.connect(client_id.c_str(), NULL, NULL)){
+            Serial.println("Public emqx mqtt broker connected");
+        }
+        else{
+            Serial.print("Failed");
+            Serial.print(client.state());
+        }
+    }
+    client.publish(topic, "Testing");
+    client.subscribe(topic);
+
+ 
+ Serial.begin(115200);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.println("Connecting to WiFi..");
+   
+client.setServer(mqtt_broker, mqtt_port);
+client.setCallback(callback);
+while (!client.connected()) {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+         Serial.println("Public emqx mqtt broker connected");
+     } else {
+         Serial.print("failed with state ");
+         Serial.print(client.state());
+         delay(2000);
 
 void loop() {
   digitalWrite(LED_RED1, HIGH); 
